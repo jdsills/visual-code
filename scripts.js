62c4cd386450b7abed3b6e651974fb729b92b1c0
jdsills <jdsills@mavs.coloradomesa.edu>
2021-10-05 16:40:20 -0600

    new commit update

--- scripts.js
+++ scripts.js
@@ -1,5 +1,21 @@
+var a
+var b
+
+a=5
+b=3
+
+function sum(){
+    var c
+    c = a+b
+    console.log('starting sum function')
+    a = a + b
+    console.log('finished sum function')
+}
+
 function givemessage(){
 
-    alert("sample alert message")
-
+    // comment
+    console.log("giving message: a=" + a)
+    console.log("c=" + c)
 }
+
